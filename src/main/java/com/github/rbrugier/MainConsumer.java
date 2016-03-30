203fd07f93c7d6b54d1ab453778f8eaa29240461
Raphael Brugier <raphael.brugier@gmail.com>
2016-03-30 15:16:38 -0400

    examples

--- src/main/java/com/github/rbrugier/MainConsumer.java
+++ src/main/java/com/github/rbrugier/MainConsumer.java
@@ -9,22 +9,25 @@ import java.util.Properties;
 
 public class MainConsumer {
 
+    public static final String TOPIC_NAME = "test3partitions";
+    public static final String GROUP_NAME = "mygroup2";
 
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "mygroup");
+        props.put("group.id", GROUP_NAME);
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList("test"));
+        consumer.subscribe(Arrays.asList(TOPIC_NAME));
 
         boolean running = true;
         while (running) {
             ConsumerRecords<String, String> records = consumer.poll(100);
             for (ConsumerRecord<String, String> record : records) {
-                System.out.println(record.value());
+                int partitionId = record.partition();
+                System.out.println("partition id = " + partitionId + " value = " + record.value());
             }
         }
 
--- src/main/java/com/github/rbrugier/MainProducer.java
+++ src/main/java/com/github/rbrugier/MainProducer.java
@@ -2,14 +2,17 @@ package com.github.rbrugier;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.PartitionInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Properties;
 
 public class MainProducer {
 
     private static final Logger logger = LoggerFactory.getLogger(MainProducer.class);
+    public static final String TOPIC_NAME = "test3partitions";
 
     public static void main(String[] args) {
         Properties props = new Properties();
@@ -20,14 +23,18 @@ public class MainProducer {
         logger.info("starting");
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        List<PartitionInfo> testTopicPartitions = producer.partitionsFor(TOPIC_NAME);
+
+        System.out.println("partitions number = " + testTopicPartitions.size());
+
         for (int i = 0; i < 100; i++) {
-            ProducerRecord<String, String> record = new ProducerRecord<>("test", "value-" + i);
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "value-" + i);
             producer.send(record);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
         }
 
         producer.close();
